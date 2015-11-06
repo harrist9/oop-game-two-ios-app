fed47ed76b2af853cbc78e6a6d19dd4113e6241c
Thomas <harrist9@icloud.com>
2015-11-05 21:00:01 -0500

    Initial Commit

---
+++ oop-game-twoTests/oop_game_twoTests.swift
@@ -0,0 +1,36 @@
+//
+//  oop_game_twoTests.swift
+//  oop-game-twoTests
+//
+//  Created by Thomas on 11/5/15.
+//  Copyright © 2015 Thomas Harris. All rights reserved.
+//
+
+import XCTest
+@testable import oop_game_two
+
+class oop_game_twoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
