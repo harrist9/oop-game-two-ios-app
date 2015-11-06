2046b5099a481ec0165015b8efd96cd3f4b60ed9
Thomas <harrist9@icloud.com>
2015-11-05 21:43:59 -0500

    updated all files

--- oop-game-two/ViewController.swift
+++ oop-game-two/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    //Outlets
+    
+    @IBOutlet weak var player1Btn: UIButton!
+    @IBOutlet weak var player2Btn: UIButton!
+    @IBOutlet weak var player1BtnLbl: UILabel!
+    @IBOutlet weak var player2BtnLbl: UILabel!
+    @IBOutlet weak var msgLbl: UILabel!
+    @IBOutlet weak var player1Img: UIImageView!
+    @IBOutlet weak var player2Img: UIImageView!
+    
+   //Variable & Properties
+    
+    
+    
+    
 
+
+    
+    //Actions
+    
+    @IBAction func player1AttackBtnPressed(sender: AnyObject) {
+        
+    }
+    
+
+    @IBAction func player2AttackBtnPressed(sender: AnyObject) {
+        
+    }
 
 }
 
